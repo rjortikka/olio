47d57f17eb7768d6620ac72aee2abad06ce9b3a6
rjortikka <60109352+rjortikka@users.noreply.github.com>
2020-02-04 14:48:49 +0200

    rework
    kaik uusiks juu

--- week2/src/week2/Dog.java
+++ week2/src/week2/Dog.java
@@ -1,10 +1,29 @@
 package week2;
 
-public class Dog {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class Dog{
+	
+	private String name;
+	private String says;
+	
+	public void speak(String says) {
+		
+		if (says.trim().isEmpty()) {
+			this.says = "Much wow";
+		} else {
+			this.says = says;
+		}
+		System.out.println(name + ": " + this.says);
+		
+	}
+	
+	public Dog(String name) {
+		if (name.trim().isEmpty()) {
+			this.name = "Doge";
+		} else {
+			this.name = name;
+		}
+		System.out.println("Hey my name is: " + this.name);
 	}
 
 }
+
--- week2/src/week2/Mainclass.java
+++ week2/src/week2/Mainclass.java
@@ -1,11 +1,27 @@
 package week2;
 
+import java.util.Scanner;
+
 public class Mainclass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hei");
-
+		String name = "";
+		String says = "";
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("Give a name to the dog: ");
+		name = scan.nextLine();
+		
+		Dog dog = new Dog(name);
+		
+		while (says.trim().isEmpty()) {
+			System.out.print("What does a dog say: ");
+			says = scan.nextLine();
+			dog.speak(says);
+		}
+		
+		scan.close();
 	}
 
 }
